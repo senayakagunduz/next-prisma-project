54ce30508d6329eceb78a3636ad4b092d8ec7679
senay.akagunduz <senay.akagunduz@semper-tech.com>
2024-10-30 15:37:59 +0300

    ..

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -15,53 +15,53 @@
 // import { IconButton } from '@/components/form/Buttons';
 // import FormContainer from '@/components/form/FormContainer';
 
-async function AdminProductsPage() {
-  // const items = await fetchAdminProducts();
-  // if (items.length === 0) return <EmptyList />;
+// async function AdminProductsPage() {
+//   // const items = await fetchAdminProducts();
+//   // if (items.length === 0) return <EmptyList />;
 
-  return (
-    <section>
-      {/* <Table>
-        <TableCaption className='capitalize'>
-          total products : {items.length}
-        </TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Product Name</TableHead>
-            <TableHead>Company</TableHead>
-            <TableHead>Price</TableHead>
-            <TableHead>Actions</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {items.map((item) => {
-            const { id: productId, name, company, price } = item;
-            return (
-              <TableRow key={productId}>
-                <TableCell>
-                  <Link
-                    href={`/products/${productId}`}
-                    className='underline text-muted-foreground tracking-wide capitalize'
-                  >
-                    {name}
-                  </Link>
-                </TableCell>
-                <TableCell>{company}</TableCell>
-                <TableCell>{formatCurrency(price)}</TableCell>
-                <TableCell className='flex items-center gap-x-2'>
-                  <Link href={`/admin/products/${productId}/edit`}>
-                    <IconButton actionType='edit' />
-                  </Link>
-                  <DeleteProduct productId={productId} />
-                </TableCell>
-              </TableRow>
-            );
-          })}
-        </TableBody>
-      </Table> */}
-    </section>
-  );
-}
+//   return (
+//     <section>
+//       {/* <Table>
+//         <TableCaption className='capitalize'>
+//           total products : {items.length}
+//         </TableCaption>
+//         <TableHeader>
+//           <TableRow>
+//             <TableHead>Product Name</TableHead>
+//             <TableHead>Company</TableHead>
+//             <TableHead>Price</TableHead>
+//             <TableHead>Actions</TableHead>
+//           </TableRow>
+//         </TableHeader>
+//         <TableBody>
+//           {items.map((item) => {
+//             const { id: productId, name, company, price } = item;
+//             return (
+//               <TableRow key={productId}>
+//                 <TableCell>
+//                   <Link
+//                     href={`/products/${productId}`}
+//                     className='underline text-muted-foreground tracking-wide capitalize'
+//                   >
+//                     {name}
+//                   </Link>
+//                 </TableCell>
+//                 <TableCell>{company}</TableCell>
+//                 <TableCell>{formatCurrency(price)}</TableCell>
+//                 <TableCell className='flex items-center gap-x-2'>
+//                   <Link href={`/admin/products/${productId}/edit`}>
+//                     <IconButton actionType='edit' />
+//                   </Link>
+//                   <DeleteProduct productId={productId} />
+//                 </TableCell>
+//               </TableRow>
+//             );
+//           })}
+//         </TableBody>
+//       </Table> */}
+//     </section>
+//   );
+// }
 
 //function DeleteProduct({ productId }: { productId: string }) {
 //   function DeleteProduct() {
