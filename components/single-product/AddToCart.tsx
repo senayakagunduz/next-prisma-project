525468ae75cc01e602944e8acd3434ad62d49e71
senay.akagunduz <senay.akagunduz@semper-tech.com>
2025-02-10 09:27:27 +0300

    fixed 2

--- components/single-product/AddToCart.tsx
+++ components/single-product/AddToCart.tsx
@@ -5,7 +5,7 @@ import { Button } from '../ui/button'
   function AddToCart({productId}:{productId:string}) {
   return (
     <Button className='capitalize mt-8' size='lg'>
-      add to cart
+      add to cart[{productId}]
     </Button>
   )
 }
--- components/single-product/ProductRating.tsx
+++ components/single-product/ProductRating.tsx
@@ -9,7 +9,7 @@ import { FaStar } from 'react-icons/fa';
   return (
     <span className={className}>
       <FaStar className='w-3 h-3' />
-      {rating} {countValue}
+      {rating} {countValue}{productId}
     </span>
   );
 }
